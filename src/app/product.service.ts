e6695554fc6583bc78f6abc68d45c3b548ce8c88
expertzlab <contact@expertzlab.com>
2019-12-30 13:44:11 +0530

    added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,6 +8,8 @@ import { IProduct } from './product';
 
 import { Observable, Subject } from 'rxjs';
 import 'rxjs/add/operator/map';
+import { map ,tap} from 'rxjs/operators';
+
 import 'rxjs/add/operator/catch';
 
 
@@ -19,18 +21,24 @@ export class ProductService {
   private _producturl = './product.json';
   constructor(private _http: HttpClient) { }
 
-  getproducts() {
+  getproducts_old() {
     //alert('hello')
     return this._http.get(this._producturl)
-    .map((response: Response) => {
-     
-      var myJSON1 = JSON.stringify(response);
-      console.log('status is' + myJSON1);
-      return response
-    })
+      .map((response: Response) => {
+
+        var myJSON1 = JSON.stringify(response);
+        console.log('status is' + myJSON1);
+        return response
+      })
 
     //return this._http.get(this._producturl).pipe( map((data) => <IProduct[]>data),
-      //tap(data => console.log(JSON.stringify(data)));
+    //tap(data => console.log(JSON.stringify(data)));
   }
+  getproducts(): Observable<IProduct[]> {
+    return this._http.get(this._producturl).pipe(
+      map((data) => <IProduct[]>data),
+      tap(data => console.log(JSON.stringify(data))));
+  }
+
 }
 
