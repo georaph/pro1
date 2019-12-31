2f83393dfd3b9f76732af9536eb101c4aa46829b
expertzlab <contact@expertzlab.com>
2019-12-31 18:16:26 +0530

    added

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,150 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { take, map, combineAll } from 'rxjs/operators';
+import { interval } from 'rxjs';
 
-import { Observable } from 'rxjs';
-
-import { of } from 'rxjs';
-import { first,filter } from 'rxjs/operators';
-
-import { ajax } from 'rxjs/ajax';
-import { map, catchError } from 'rxjs/operators';
-import 'rxjs/add/observable/from';
-//import 'rxjs/add/observable/filter';
-//import { MyObserver } from './MyObserver'
-
-
-
-@Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
-})
-
-
-export class RxjsComponent implements OnInit {
-
-  constructor() {
-
-    const observable = new Observable(subscriber => {
-      subscriber.next(2);
-      subscriber.next(20);
-      subscriber.next(3);
-      subscriber.next(4);
-      setTimeout(() => {
-        subscriber.next(5);
-        subscriber.complete();
-      }, 0);
-      subscriber.next(14);
-    });
-
-    console.log('just before subscribe');
-    observable.subscribe({
-      next(x) {
-        for (var i = 0; i < x; i++)
-          console.log('got value== ' + i);
-      },
-      error(err) { console.error('something wrong occurred: ' + err); },
-      complete() { console.log('done'); }
-    });
-    console.log('just after subscribe');
-
-
-
-
-    const foo = new Observable(subscriber => {
-      console.log('Hello');
-      subscriber.next(42);
-    });
-
-    foo.subscribe(x => {
-      console.log('-----------' + x);
-    });
-    // foo.subscribe(y => {
-    //   console.log(y);
-    // });
-
-    const obs = ajax('http://localhost:3000/employees').pipe(
-      map(userResponse => console.log('users: ',
-        JSON.stringify(
-          userResponse))),
-      catchError(error => {
-        console.log('error: ', error);
-        return of(error);
-      })
-    );
-
-
-    obs.subscribe(x => {
-      console.log('-------------------------' + x);
-    });
-
-
-
-    of(420).subscribe(
-      val => {
-        console.log(val); //-> prints 42
-      }
-    );
-    of(1, 2, 3, 4, 5).subscribe(
-      val => {
-        console.log(val);
-      }
-    );
-   var a
-    var ars=new Array()
-    of(a).subscribe(
-      value => console.log(value),
-      error => console.log(error),
-      () => console.log("complete")
-    );
-
-
-
-    let numbers = [3,9,7];
-     source = Observable.from(numbers).map(value => {
-      return 2*value;
-    })
-    source.subscribe(value => {
-      console.log(value);
-    })
-
-
-    var source = Observable.create(observer => {
-      observer.next('Ram')
-      observer.next('Tom')
-      observer.next('Hary')
-      observer.complete()
-    });
-     source.do(//value => console.log(value)
+// emit every 1s, take 2
+const source$ = interval(1000).pipe(take(2));
+// map each emitted value from source to interval observable that takes 5 values
+const example$ = source$.pipe(
+  map(val =>
+    interval(1000).pipe(
+      map(i => `Result (${val}): ${i}`),
+      take(5)
     )
-        .map(value => value.toLowerCase())
-        //.map(value => value.toLowerCase())
-        //.do(value => console.log(value))
-        //.do(value => console.log(value))
-        .subscribe(
-        //value => console.log(value),
-        error => console.log(error),
-        () => console.log("complete")
-      );
-
-
-
-
-
-      var  names = ['Ram', 'Tom', 'Hary', 'Hem'];
-    //   Observable.from(names).filter(name => name.startsWith('H'))
-    //        .subscribe(name => {
-    //          console.log(name)
-    //        })
-
-
-    // Observable.from(names).first()
-    // .subscribe(name => {
-    //   console.log(name)
-    // })
-     
-  }
-
-  ngOnInit() {
-  }
-
-}
+  )
+);
+/*
+  2 values from source will map to 2 (inner) interval observables that emit every 1s.
+  combineAll uses combineLatest strategy, emitting the last value from each
+  whenever either observable emits a value
+*/
+example$
+  .pipe(combineAll())
+  /*
+  output:
+  ["Result (0): 0", "Result (1): 0"]
+  ["Result (0): 1", "Result (1): 0"]
+  ["Result (0): 1", "Result (1): 1"]
+  ["Result (0): 2", "Result (1): 1"]
+  ["Result (0): 2", "Result (1): 2"]
+  ["Result (0): 3", "Result (1): 2"]
+  ["Result (0): 3", "Result (1): 3"]
+  ["Result (0): 4", "Result (1): 3"]
+  ["Result (0): 4", "Result (1): 4"]
+*/
+  .subscribe(console.log);
